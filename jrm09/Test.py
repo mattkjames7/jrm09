11f5efaa38206bde7ea467a9fb1bcbaa86d0a80f
mattkjames7 <mattkjames7@gmail.com>
2022-01-04 12:55:23 +0000

    scalar function is faster

--- jrm09/Test.py
+++ jrm09/Test.py
@@ -1,6 +1,8 @@
 import numpy as np
-from .Model import Model
+from .Model import Model,ModelScalar
 import time
+import pstats
+import cProfile
 
 def Test(R=0.85,MaxDeg=10):
 	'''
@@ -84,31 +86,139 @@ def Timing(R=0.85,MaxDeg=10):
 	longcr = longc*np.pi/180.0
 	latgcr = (90.0 - latgc)*np.pi/180.0
 	r = np.zeros(longcr.shape) + R
+	
+	r = r.flatten()
+	longcr = longcr.flatten()
+	latgcr = latgcr.flatten()
 
 	
 	print('Timing 64800 model vectors')
 	t0 = time.time()
 
-	for i in range(0,10):
-		#calculate the model
-		Br,Bt,Bp = Model(r,latgcr,longcr,MaxDeg)
+	#calculate the model
+	Br,Bt,Bp = Model(r,latgcr,longcr,MaxDeg)
 		
 	t1 = time.time()
-	print('Completed in {:f}s'.format((t1-t0)/10.0))
+	print('Completed in {:f}s'.format(t1-t0))
 
-	r0 = np.random.rand()*5 + 1.0
-	lat0 = np.random.rand()*np.pi
-	lon0 = np.random.rand()*np.pi*2
 
 	
-	print('Timing 10000 individual vectors')
+	print('Timing 64800 individual vectors (scalar code)')
 	t0 = time.time()
 
-	for i in range(0,10000):
+	for i in range(0,r.size):
 		#calculate the model
-		Br,Bt,Bp = Model(r0,lat0,lon0,MaxDeg)
+		Br,Bt,Bp = ModelScalar(r[i],latgcr[i],longcr[i],MaxDeg)
 		
 	t1 = time.time()
-	print('Completed in {:f}s'.format((t1-t0)/10000.0))	
+	print('Completed in {:f}s'.format(t1-t0))	
 	
 	
+	print('Timing 64800 individual vectors')
+	t0 = time.time()
+
+	for i in range(0,r.size):
+		#calculate the model
+		Br,Bt,Bp = Model(r[i],latgcr[i],longcr[i],MaxDeg)
+		
+	t1 = time.time()
+	print('Completed in {:f}s'.format(t1-t0))	
+	
+
+
+def _GetRTP(R=0.85,MaxDeg=10):
+	#get the coordinates to calculate the model at
+	lat = np.linspace(-90,90,181)
+	lon = np.linspace(0.0,360.0,361)
+	latc = 0.5*(lat[1:] + lat[:-1])
+	lonc = 0.5*(lon[1:] + lon[:-1])
+	long,latg = np.meshgrid(lon,lat)
+	longc,latgc = np.meshgrid(lonc,latc)
+
+	longcr = longc*np.pi/180.0
+	latgcr = (90.0 - latgc)*np.pi/180.0
+	r = np.zeros(longcr.shape) + R
+	
+	r = r.flatten()
+	longcr = longcr.flatten()
+	latgcr = latgcr.flatten()
+	
+	return r,longcr,latgcr
+	
+def _CallVectorized(r,t,p,MaxDeg):
+
+		#calculate the model
+		Br,Bt,Bp = Model(r,t,p,MaxDeg)
+
+	
+def _CallVectorizedLoop(r,t,p,MaxDeg):
+
+	for i in range(0,r.size):
+		#calculate the model
+		Br,Bt,Bp = Model(r[i],t[i],p[i],MaxDeg)		
+	
+	
+def _CallScalarLoop(r,t,p,MaxDeg):
+
+	for i in range(0,r.size):
+		#calculate the model
+		Br,Bt,Bp = ModelScalar(r[i],t[i],p[i],MaxDeg)		
+	
+def TimeVectorized():
+
+	print('Timing 64800 model vectors (Vectorized Code)')
+	#_CallVectorized()
+	
+	#cProfile.run("_CallVectorized",'restats')
+	#p = pstats.Stats('restats')
+	#p.strip_dirs().sort_stats(2).print_stats()
+
+	R = 0.85
+	MaxDeg = 10
+	r,t,p = _GetRTP(R,MaxDeg)
+	
+	pr = cProfile.Profile()
+	pr.enable()
+	_CallVectorized(r,t,p,MaxDeg)
+	pr.disable()
+	stats = pstats.Stats(pr).strip_dirs().sort_stats('cumtime')
+	stats.print_stats()
+	
+	
+
+	
+def TimeVectorizedLoop(R=0.85,MaxDeg=10):
+
+
+	#get the coordinates to calculate the model at
+
+	print('Timing 64800 model vectors (Vectorized Code, Looping through individual vectors)')
+	
+	R = 0.85
+	MaxDeg = 10
+	r,t,p = _GetRTP(R,MaxDeg)
+	
+	pr = cProfile.Profile()
+	pr.enable()
+	_CallVectorizedLoop(r,t,p,MaxDeg)
+	pr.disable()
+	stats = pstats.Stats(pr).strip_dirs().sort_stats('cumtime')
+	stats.print_stats()
+	
+	
+	
+def TimeScalarLoop(R=0.85,MaxDeg=10):
+
+
+	print('Timing 64800 model vectors (Scalar Code, Looping through individual vectors)')
+	
+	R = 0.85
+	MaxDeg = 10
+	r,t,p = _GetRTP(R,MaxDeg)
+	
+	pr = cProfile.Profile()
+	pr.enable()
+	_CallScalarLoop(r,t,p,MaxDeg)
+	pr.disable()
+	stats = pstats.Stats(pr).strip_dirs().sort_stats('cumtime')
+	stats.print_stats()
